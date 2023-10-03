1d93e50f39c0ea2a4492db2c7121128308d2eb75
Wikcio1618 <ciolekwiktor@gmail.com>
2023-10-03 19:00:38 +0200

    first commit

--- README.md
+++ README.md
@@ -4,15 +4,8 @@ Implementation of a group creativity model with visualisation and Gui for engine
 ---
 
 ### Universal rules
-There are N agents that assume discrete positions in 1D space. All agents are performing a random walk with a dynamic jump(step) length.
-There are also N target positions randomly selected. The goal of agents is to assume all targets. If one steps on it his step is permanently set to 0. There are 2 types of agents: creative (g) and conservative (1-g). After each time step (day) the feedback function is calculated: $d(t_i)=\sqrt{\frac{\sum_{n=0}^N{x^2_n}}{N}}$, where $x_n$ is distance of agent to the nearest free target. Agents that found a target don't contribute to this function.
+
 
 ### Distinct rules that may differ in other branches
-1. Agents start at random positions
-2. Space is looped
-3. Each creative agent at the end of i-th day increases step of agents in range r by F, PROVIDED THAT d(t_i) is greater than their step
-4. Conservative agents interact in a symmetrical way
 
 
-## TODO
-Wykres w funkcji czasu (wszyscy i kreatywni/konserwaty)
--- agent.py
+++ agent.py
@@ -11,20 +11,28 @@ class Agent():
     # creativity - float
     # openness
 
-    def __init__(self, creativity = 0, openness = 0):
-        self.creativity = creativity
-        self.openness = openness
+	def __init__(self, creativity = 0, openness = 0, resource = 1):
+		self.creativity = creativity
+		self.openness = openness
+		self.resource = resource
 
-    def add_creativity(self, crea):
-        self.creativity += crea
-        if self.creativity < 0:
-            self.creativity = 0
-        elif self.creativity > 1:
-            self.creativity = 1
+	def add_creativity(self, crea):
+		self.creativity += crea
+		if self.creativity < 0:
+			self.creativity = 0
+		elif self.creativity > 1:
+			self.creativity = 1
 
-    def add_openness(self, open):
-        self.openness += open
-        if self.openness < 0:
-            self.openness = 0
-        elif self.openness > 1:
-            self.openness = 1
+	def add_openness(self, op):
+		self.openness += op
+		if self.openness < 0:
+			self.openness = 0
+		elif self.openness > 1:
+			self.openness = 1
+			
+	def add_resource(self, res):
+		self.resource += res
+		if self.resource < 0:
+			self.resource = 0
+		elif self.resource > 1:
+			self.resource = 1
