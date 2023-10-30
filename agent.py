53788e5771730b209574e2581d411383da3c3b86
Wikcio1618 <ciolekwiktor@gmail.com>
2023-10-30 13:44:13 +0100

    new model where creativity of 2 agents is compared

--- agent.py
+++ agent.py
@@ -1,20 +1,9 @@
-from enum import Enum
-
-
-class AgentType(Enum):
-    CREATIVE = 'creative'
-    SUPPORTIVE = 'supportive'
-
-
 class Agent():
     # FIELDS:
     # creativity - float
-    # openness
 
-	def __init__(self, creativity = 0, openness = 0, resource = 1):
+	def __init__(self, creativity = 0):
 		self.creativity = creativity
-		self.openness = openness
-		self.resource = resource
 
 	def add_creativity(self, crea):
 		self.creativity += crea
@@ -22,17 +11,3 @@ class Agent():
 		# 	self.creativity = 0
 		# elif self.creativity > 1:
 		# 	self.creativity = 1
-
-	def add_openness(self, op):
-		self.openness += op
-		# if self.openness < 0:
-		# 	self.openness = 0
-		# elif self.openness > 1:
-		# 	self.openness = 1
-			
-	def add_resource(self, res):
-		self.resource += res
-		if self.resource < 0:
-			self.resource = 0
-		elif self.resource > 1:
-			self.resource = 1
--- society.py
+++ society.py
@@ -1,71 +1,58 @@
 from math import floor, sqrt, exp
 import numpy as np
-from numpy import array
 from random import randint, choice
 
-from agent import Agent, AgentType
+from agent import Agent
 
 class Society:
     # FIELDS:
     # agents - array of agents
     # day - time iterations
-    # progress
-	# num_of_tasks how much tasks can one agent do
 
-	def __init__(self, crea_dist = None, open_dist = None, num_of_agents=100
-			  , creativity_force=0, openness_force=0, temperature=0):
-		self.num_of_agents=num_of_agents
-		if crea_dist is None:
-			crea_dist  = np.random.normal(0.5, 0.15, self.num_of_agents)
-		self.crea_dist = crea_dist
-		if open_dist is None:
-			open_dist = np.random.normal(0.5, 0.15, self.num_of_agents)
-		self.open_dist = open_dist
+	def __init__(self, crea_mean=10, crea_stddev=0.2, num_of_agents=100, creativity_force=0, thresh=None, temperature=0):
+		self.num_of_agents = num_of_agents
+		self.crea_dist  = np.random.normal(crea_mean, crea_stddev, self.num_of_agents)
 		self.creativity_force = creativity_force
-		self.openness_force = openness_force
+		if thresh is None:
+			thresh = crea_mean * crea_mean
+		self.thresh = thresh
 		self.temperature = temperature
-		self.new_society(self.crea_dist, self.open_dist)
+		self.new_society(self.crea_dist)
 
-	def new_society(self, crea_dist, open_dist):
-		self.progress = 0
+	def new_society(self, crea_dist):
 		self.day = 0
-		
-		# create agents and assign creativity and openness according to distributions
-		self.agents = array([Agent(creativity=crea_dist[i], openness=open_dist[i]) for i in range(self.num_of_agents)])
+		# create agents and assign creativity according to distributions
+		self.agents = np.array([Agent(creativity=crea_dist[i]) for i in range(self.num_of_agents)])
 
 	def next_step(self):
 		# find idea_giver and neighbour
 		considered_agents = set([])
-		idea_giver = choice(self.agents)
-		considered_agents.add(idea_giver)
+		agent_i = choice(self.agents)
+		considered_agents.add(agent_i)
 		while len(considered_agents) < 2:
-			voter = choice(self.agents)
-			considered_agents.add(voter)
+			agent_j = choice(self.agents)
+			considered_agents.add(agent_j)
 
-		vote_accepted=(np.random.uniform() < self.sigmoid(voter.openness - idea_giver.creativity, self.temperature))
+		vote_accepted=(np.random.uniform() < self.sigmoid(agent_j.creativity * agent_i.creativity - self.thresh, self.temperature))
 		if vote_accepted:
-			# idea accepted
-			self.progress += 1 #TODO temporary
-			voter.add_openness(-self.openness_force)
-			idea_giver.add_creativity(+self.creativity_force)
+			agent_i.add_creativity(+self.creativity_force)
+			agent_j.add_creativity(+self.creativity_force)
 		else:
-			idea_giver.add_creativity(-self.creativity_force)
-			voter.add_openness(+self.openness_force)
+			agent_i.add_creativity(-self.creativity_force)
+			agent_j.add_creativity(-self.creativity_force)
 
 		self.day += 1
 	
-	# runs society and returns number of days
-	def run_until_target(self, max_days = 10**3, target_progress=5) -> int:
-		self.new_society()
-		while self.progress < target_progress and self.day < max_days:
-			self.next_step()
-		return self.day
-
-	def run_until_time(self, time=100) -> int:
-		self.new_society(self.crea_dist, self.open_dist)
+	def run_until_time(self, time):
+		self.new_society(self.crea_dist)
 		for _ in range(time):
 			self.next_step()
-		return self.progress
+
+	def get_crea_mean(self):
+		return np.average([x.creativity for x in self.agents])
+
+	def get_crea_stddev(self):
+		return np.std([x.creativity for x in self.agents])
 
 	@staticmethod
 	def sigmoid(x, T):
